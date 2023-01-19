af7f4c0bf5cd7c95725d4eebe3836f7d0a1e4094
Prince <princev2451@gmail.com>
2023-01-19 11:55:43 +0530

    css files added in styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import registerEcharts from './echarts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import registerEcharts from "./echarts";
 
 // registering echarts components
 registerEcharts();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
